60020eef8b93bef959a40728b4a3e9e0b957d348
adityanpatil15 <107454254+adityanpatil15@users.noreply.github.com>
2024-09-13 23:08:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const tbody = document.querySelector('.tr')
+// for(i=1 ;i<=31 ; i++)
+// {
+//     console.log(i)
+//     const newtr = document.createElement('td')
+//     newtr.innerHTML = 
+//     `<span>${i}</span>`
+//     tbody.appendChild(newtr)
+// }
